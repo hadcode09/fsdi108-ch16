5d98483f6a6cc05dfeb48095419942c2773d99c5
Mark Hadley <mark.hadley1@hotmail.com>
2021-03-22 19:58:10 -0700

    FSDI108 class 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import './App.css';
 import Footer from './component/footer';
 import NavBar from './component/navBar';
+import Product from './component/product';
+import Catalog from './component/catalog';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import Product from './component/product';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
 
-      <h1>Hello World!</h1>
+      <Catalog></Catalog>
 
-
-
-    <Product></Product>
-    <Product></Product>
-    <Product></Product>
-    <Product></Product>
-    <Product></Product>
-    <Product></Product>
+    
 
 
     <Footer></Footer>
--- src/component/product.jsx
+++ src/component/product.jsx
@@ -9,12 +9,12 @@ class Product extends Component {
         return (
             <div className="product">
 
-                <img src="https://picsum.photos/200/300"></img>
+                <img className="product-image"src={"/images/products/"+this.props.data.image}></img>
 
-                <label>Product Description here</label>
+                <label>{this.props.data.title}</label>
 
-                <label>$ Total</label>
-                <label>$ Price</label>
+                <label>$ {this.props.data.price}</label>
+                <label>${this.props.data.price}</label>
                 <QuantityPicker></QuantityPicker>
             </div>
         )
